e1adc84584fb1461d4bdc54d56a1d3898d400df2
nullptr-c-cpp <318332592+nullptr-c-cpp@users.noreply.github.com>
2026-08-25 21:29:09 +0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   // 1. Ссылка на ваш GitHub Pages (замените на ваш логин):
-  site: 'https://nullptr-c-cpp.github.io',
+  site: 'https://bironiks.ru',
 
   // 2. Имя вашего репозитория со слэшем (например: '/my-bironiks-site'):
   base: '/',
